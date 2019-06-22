92f43060c9d8caa23eb59ef87b1eaa4c20f2d0ea
ChristianMacDonald <chrismacd412@gmail.com>
2019-06-22 12:45:22 -0400

    Added more boilerplate

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    todos: []
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;

5af29dcb4a511b71dc5da0157f413ed9ae03232e
ChristianMacDonald <chrismacd412@gmail.com>
2019-06-22 13:14:38 -0400

    Added functionality to create new todos

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addTodo } from '../actions';
 
-const TodoForm = props => {
-    return (
-        <form>
-            <input type="text" name="value" />
-            <input type="submit" />
-        </form>
-    );
+
+
+class TodoForm extends React.Component {
+    handleSubmit = event => {
+        event.preventDefault();
+        let elements = event.target.elements;
+        let todo = {
+            value: elements['value'].value,
+            completed: false
+        };
+        this.props.addTodo(todo);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="value" />
+                <input type="submit" />
+            </form>
+        );
+    }
 }
 
 const mapStateToProps = state => {
     return {};
 }
 
-export default connect(mapStateToProps, {})(TodoForm);
+export default connect(mapStateToProps, { addTodo: addTodo })(TodoForm);
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,9 +1,13 @@
+import { ADD_TODO } from '../actions';
+
 const initialState = {
     todos: []
 };
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+        case ADD_TODO:
+            return { todos: state.todos.concat(action.payload) };
         default:
             return state;
     }
